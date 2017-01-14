969cef7c2ba9993303e7be5c96a63eb81fa69f1b
tjjohn1 <tjjohn1@asu.edu>
2017-01-14 10:08:38 -0700

    Update Application.java
    Builder for json object from returned json formatted string, 
    also prints to the console a pretty print formatted version of that new object

--- existing-rest-request/src/main/java/existingRest/Application.java
+++ existing-rest-request/src/main/java/existingRest/Application.java
@@ -1,5 +1,7 @@
 package existingRest;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Application {
@@ -24,11 +30,13 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Users users = restTemplate.getForObject(
-					"https://slack.com/api/users.list?token=xoxp-83385818629-83396174567-123161160469-710d4bbb79ee564ee258650e455f9a7f&pretty=1", Users.class);
-			log.info(users.toString());
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception, JsonParseException, IOException {
+		return args -> { String result = restTemplate.getForObject("https://slack.com/api/users.list?token=xoxp-83385818629-83396174567-123161160469-710d4bbb79ee564ee258650e455f9a7f&pretty=1", String.class);
+	 
+	    		ObjectMapper mapper = new ObjectMapper();
+	    		JsonNode actualObj = mapper.readTree(result);
+	    	
+	    		System.out.print(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(actualObj));
 		};
 	}
 }
