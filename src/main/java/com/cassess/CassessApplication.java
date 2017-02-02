6833c5ef2be0cd280d042fdd942df1c75c68dce1
ImCesar <cesaravitia@outlook.com>
2017-02-01 23:16:06 -0700

    Added support to gather all branches

--- src/main/java/com/cassess/CassessApplication.java
+++ src/main/java/com/cassess/CassessApplication.java
@@ -1,7 +1,6 @@
 package com.cassess;
 
-import com.cassess.model.taiga.ConsumeAuthUser;
-import com.cassess.model.taiga.ConsumeProjectList;
+import com.cassess.model.github.GatherGitHubData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -65,20 +65,11 @@ public class CassessApplication {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-            //ConsumeUsers consumeUsers = (ConsumeUsers) ctx.getBean("consumeUsers");
-    		//consumeUsers.getUserInfo("U2G79FELT");
+            ConsumeUsers consumeUsers = (ConsumeUsers) ctx.getBean("consumeUsers");
+    		consumeUsers.getUserInfo("U2G79FELT");
 
-            ConsumeAuthUser consumeAuthUser = (ConsumeAuthUser) ctx.getBean("consumeAuthUser");
-            consumeAuthUser.getUserInfo();
-            String token = consumeAuthUser.getToken("TaigaTestUser@gmail.com");
-            System.out.println("Taiga Token: " + token);
-            Long id = consumeAuthUser.getID("TaigaTestUser@gmail.com");
-            System.out.println("Taiga Member ID: " + id);
-
-            ConsumeProjectList consumeProjectList = (ConsumeProjectList) ctx.getBean("consumeProjectList");
-            consumeProjectList.getProjectInfo(token, id);
-            System.out.println("Taiga Project Name: " + consumeProjectList.getName("tjjohn1"));
-
+            GatherGitHubData gatherGitHubData = (GatherGitHubData) ctx.getBean("gatherGitHubData");
+            gatherGitHubData.fetchData();
         };
     }
     
--- src/main/java/com/cassess/model/github/GitHubApplication.java
+++
@@ -1,9 +0,0 @@
-package com.cassess.model.github;
-
-public class GitHubApplication {
-
-    public static void main(String[] args){
-        GatherGitHubData gitHubData = new GatherGitHubData();
-        gitHubData.fetchData();
-    }
-}
