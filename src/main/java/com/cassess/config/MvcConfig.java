c7575d5a47661b69b6c77564e8943863c1a46179
cmoretti <cmoretti@asu.edu>
2017-03-13 20:42:55 -0400

    Reverted some front end changes as a test

--- src/main/java/com/cassess/config/MvcConfig.java
+++ src/main/java/com/cassess/config/MvcConfig.java
@@ -1,51 +1,51 @@
-package com.cassess.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.GzipResourceResolver;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
-
-@Configuration
-@Import({SecurityConfig.class})
-@EnableWebMvc
-@ComponentScan(basePackages = { "com.cassess.web.controller" })
-public class MvcConfig extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-        return new ResourceUrlEncodingFilter();
-    }
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webapp/resources/vendor/**")
-                .addResourceLocations("/webapp/resources/vendor/")
-                .setCachePeriod(0)
-                .resourceChain(true)
-                .addResolver(new GzipResourceResolver())
-                .addResolver(new PathResourceResolver());
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
-
-    @Bean(name = "messageSource")
-    public ReloadableResourceBundleMessageSource getMessageSource() {
-        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
-        resource.setBasename("classpath:messages");
-        resource.setDefaultEncoding("UTF-8");
-        return resource;
-    }
-}
+//package com.cassess.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.resource.GzipResourceResolver;
+//import org.springframework.web.servlet.resource.PathResourceResolver;
+//import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+//
+//@Configuration
+//@Import({SecurityConfig.class})
+//@EnableWebMvc
+//@ComponentScan(basePackages = { "com.cassess.web.controller" })
+//public class MvcConfig extends WebMvcConfigurerAdapter {
+//
+//    @Bean
+//    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+//        return new ResourceUrlEncodingFilter();
+//    }
+//
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/webapp/resources/vendor/**")
+//                .addResourceLocations("/webapp/resources/vendor/")
+//                .setCachePeriod(0)
+//                .resourceChain(true)
+//                .addResolver(new GzipResourceResolver())
+//                .addResolver(new PathResourceResolver());
+//    }
+//
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+//
+//
+//    @Bean(name = "messageSource")
+//    public ReloadableResourceBundleMessageSource getMessageSource() {
+//        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
+//        resource.setBasename("classpath:messages");
+//        resource.setDefaultEncoding("UTF-8");
+//        return resource;
+//    }
+//}
