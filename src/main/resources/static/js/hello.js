1f185fac9ce6ecac9bcbb9d1625b59d883a6deec
psharif <psharif@asu.edu>
2017-03-21 18:57:45 -0700

    "Loads a test chart from FusionCharts to the charts.html page"

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,4 +1,4 @@
-angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProvider) {
+angular.module('hello', [ 'ngRoute', 'ng-fusioncharts' ]).config(function($routeProvider, $httpProvider) {
 
     $routeProvider.when('/', {
         templateUrl : 'partials/home.html',
@@ -213,4 +213,36 @@ angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider, $httpProv
         $http.get("/charts/").then(function (response) {
             self.chartData = response.data;
         })
-    });
+    }).controller('MyController', function ($scope) {
+
+//Define the `myDataSource` scope variable.
+    $scope.myDataSource = {
+        chart: {
+            caption: "Harry's SuperMart",
+            subCaption: "Top 5 stores in last month by revenue",
+            numberPrefix: "$",
+            theme: "fint"
+        },
+        data:[{
+            label: "Bakersfield Central",
+            value: "880000"
+        },
+            {
+                label: "Garden Groove harbour",
+                value: "730000"
+            },
+            {
+                label: "Los Angeles Topanga",
+                value: "590000"
+            },
+            {
+                label: "Compton-Rancho Dom",
+                value: "520000"
+            },
+            {
+                label: "Daly City Serramonte",
+                value: "330000"
+            }]
+    };
+});
+
