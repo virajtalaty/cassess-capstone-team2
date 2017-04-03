c19ca8cca8698dc7bf03475f45e29bd6da67b825
psharif <psharif@asu.edu>
2017-04-03 16:00:16 -0700

    Changed the html page for github to display example. Added charts controller to controller.js. Added a chartscontroller.java file for testing purposes.

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -251,4 +251,48 @@ myapp.controller('LoginController', function ($rootScope, $scope, AuthSharedServ
 
     };
 
-    } ]);
+    } ]).controller('charts', function($scope, $http) {
+        var self = this;
+        $http.get("/charts/").then(function (response) {
+            self.chartData = response.data;
+            /*$scope.myDataSource = {
+                chart: {
+                    caption: "GitHubCommits for tjjohn1",
+                    subCaption: "Displays Commits By Week",
+                    theme: "fint"
+                },
+                data: response.data
+            };*/
+        });
+        //$scope.myDataSource = {};
+
+            //Define the `myDataSource` scope variable.
+        $scope.myDataSource = {
+            chart: {
+                caption: "Harry's SuperMart",
+                subCaption: "Top 5 stores in last month by revenue",
+                numberPrefix: "$",
+                theme: "fint"
+            },
+            data:[{
+                label: "Bakersfield Central",
+                value: "880000"
+            },
+            {
+                label: "Garden Groove harbour",
+                value: "730000"
+            },
+            {
+                label: "Los Angeles Topanga",
+                value: "590000"
+            },
+            {
+                label: "Compton-Rancho Dom",
+                value: "520000"
+            },
+            {
+                label: "Daly City Serramonte",
+                value: "330000"
+            }]
+        };
+    });
