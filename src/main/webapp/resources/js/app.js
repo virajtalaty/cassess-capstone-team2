b4fc3a0a4e7ac681fcb2233e7fca286cdb43e8dc
psharif <psharif@asu.edu>
2017-04-04 21:58:21 -0700

    Displayed example for angular-nvd3 charts. In newest master from github. Need to add data to DB to display sample chart, in new version.

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var myapp = angular
-    .module('myApp', ['ngResource', 'ngRoute', 'swaggerUi', 'http-auth-interceptor', 'ngAnimate', 'angular-spinkit']);
+    .module('myApp', ['ngResource', 'ngRoute', 'swaggerUi', 'http-auth-interceptor', 'ngAnimate', 'angular-spinkit', 'nvd3']);
 
 
 myapp.constant('USER_ROLES', {
@@ -161,7 +161,6 @@ myapp.run(function ($rootScope, $location, $http, AuthSharedService, Session, US
 
     // Get already authenticated user account
     AuthSharedService.getAccount();
-
 
 });
 
