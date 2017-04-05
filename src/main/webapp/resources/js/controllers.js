6734c3b9bf6cf5835e4ce34b52a4651decc068d3
psharif <psharif@asu.edu>
2017-04-04 18:10:32 -0700

    Displayed example for angular-nvd3 charts. Still need to access data from GET mapping. Can't access it right now.

--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -253,8 +253,8 @@ myapp.controller('LoginController', function ($rootScope, $scope, AuthSharedServ
 
 } ]).controller('charts', function($scope, $http) {
         var self = this;
-        $http.get("/charts/").then(function (response) {
-            self.chartData = response.data;
+        $http.get("./charts").then(function (response) {
+            $scope.chartData = response.data;
             /*$scope.myDataSource = {
                 chart: {
                     caption: "GitHubCommits for tjjohn1",
@@ -267,7 +267,7 @@ myapp.controller('LoginController', function ($rootScope, $scope, AuthSharedServ
             //$scope.myDataSource = {};
 
             //Define the `myDataSource` scope variable.
-            $scope.myDataSource = {
+           /* $scope.myDataSource = {
                 chart: {
                     caption: "Harry's SuperMart",
                     subCaption: "Top 5 stores in last month by revenue",
@@ -294,5 +294,48 @@ myapp.controller('LoginController', function ($rootScope, $scope, AuthSharedServ
                     label: "Daly City Serramonte",
                     value: "330000"
                 }]
+            };*/
+
+            $scope.options = {
+                chart: {
+                    type: 'discreteBarChart',
+                    height: 450,
+                    margin : {
+                        top: 20,
+                        right: 20,
+                        bottom: 60,
+                        left: 55
+                    },
+                    x: function(d){ return d.label; },
+                    y: function(d){ return d.value; },
+                    showValues: true,
+                    valueFormat: function(d){
+                        return d3.format(',.4f')(d);
+                    },
+                    transitionDuration: 500,
+                    xAxis: {
+                        axisLabel: 'X Axis'
+                    },
+                    yAxis: {
+                        axisLabel: 'Y Axis',
+                        axisLabelDistance: 30
+                    }
+                }
             };
+
+            $scope.data = [{
+                key: "Cumulative Return",
+                values: [
+                    { "label" : "A" , "value" : -29.765957771107 },
+                    { "label" : "B" , "value" : 0 },
+                    { "label" : "C" , "value" : 32.807804682612 },
+                    { "label" : "D" , "value" : 196.45946739256 },
+                    { "label" : "E" , "value" : 0.19434030906893 },
+                    { "label" : "F" , "value" : -98.079782601442 },
+                    { "label" : "G" , "value" : -13.925743130903 },
+                    { "label" : "H" , "value" : -5.1387322875705 }
+                ]
+            }];
+
+
     });
